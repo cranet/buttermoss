fa3deee2eb412effa9c0c235b3ee19ba9155e281
rhaydenx <alamb25@uw.edu>
2017-01-16 12:39:15 -0800

    test2

--- test.py
+++ test.py
@@ -6,6 +6,8 @@ class TestApp(App):
     def build(self):
         return Button(text="About", background_color=(0, 0, 1, 1), font_size=150)
 
+
+
 if __name__ == "__main__":
     TestApp().run()
     
