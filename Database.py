3fcb5a407742258815f6c1c1389cbc66da681052
RhaydenX <alamb25@uw.edu>
2017-03-06 23:47:09 -0800

    initial db. mostly garbage code please ignore

---
+++ Database.py
@@ -0,0 +1,101 @@
+import sqlite3
+
+class Database(object):
+
+    def __init__(self):
+
+        self.conn = sqlite3.connect('example.db')
+        self.cursor = self.conn.cursor()
+
+    def intializeTables(self):
+        self.cursor.execute('''CREATE TABLE CONTESTANTS
+                            (ID INT PRIMARY KEY     NOT NULL,
+                            NAME            TEXT    NOT NULL,
+                            EMAIL           TEXT    NOT NULL,
+                            CATEGORIES      TEXT    NOT NULL);''')
+        self.cursor.execute('''CREATE TABLE JUDGES
+                            (ID INT PRIMARY KEY     NOT NULL,
+                            NAME            TEXT    NOT NULL,
+                            EMAIL           TEXT    NOT NULL,
+                            CATEGORIES      TEXT    NOT NULL);''')
+        self.cursor.execute('''CREATE TABLE ADMINS
+                            (ID INT PRIMARY KEY     NOT NULL,
+                            NAME            TEXT    NOT NULL,
+                            EMAIL           TEXT    NOT NULL);''')
+        self.cursor.execute('''CREATE TABLE CATEGORIES
+                            (ID INT PRIMARY KEY     NOT NULL,
+                            NAME            TEXT    NOT NULL,
+                            ABOUT           TEXT    NOT NULL,
+                            START_TIME      TEXT    NOT NULL);''')
+        # self.cursor.execute('''CREATE TABLE contestants
+        #                    (id INTEGER, name text, email text, categories text);''')
+        # self.cursor.execute('''CREATE TABLE judges
+        #                    (id INTEGER, name text, email text, categories text)''')
+        # self.cursor.execute('''CREATE TABLE admins
+        #                    (id INTEGER, name text, email text)''')
+        # self.cursor.execute('''CREATE TABLE categories
+        #                    (id INTEGER, name text, about text, startTime text)''')
+
+    def commit(self):
+        self.conn.commit()
+
+    def addContestant(self, id, name, email, categories):
+        thing = [id, name, email, categories]
+        self.cursor.execute("INSERT INTO CONTESTANTS (ID,NAME,EMAIL,CATEGORIES) \
+                            VALUES (?, ?, ?, ?)", thing)
+
+    def getContestant(self, id):
+        toReturn = self.cursor.execute('SELECT * FROM CONTESTANTS WHERE ID=?', (id,))
+        return toReturn
+
+    def getAllContestant(self):
+        toReturn = self.cursor.execute('SELECT * FROM CONTESTANTS')
+        return toReturn
+
+    def getAllContestantID(self):
+        return set(self.cursor.execute('SELECT id FROM contestants').fetchall())
+
+    def modifyContestant(self, id, t):
+        catList = " ".join(t)
+        te = [catList, id]
+        self.cursor.execute('UPDATE set categories=? where id=?', te)
+
+    def removeContestant(self, id):
+        self.conn.execute("DELETE FROM contestants WHERE id=?", (id,))
+
+    def addJudge(self, id, name, email, categories):
+        thing = [id, name, email, categories]
+        self.cursor.execute("INSERT INTO contestants VALUES (?, ?, ?, ?)", thing)
+
+    def getJudge(self, id):
+        self.cursor.execute('SELECT id, name, email, categories FROM contestants WHERE id=?', (id,))
+        return self.cursor.fetchone()
+
+    def removeJudge(self, id):
+        self.conn.execute("DELETE FROM contestants WHERE id=?", (id,))
+
+    def closeDB(self):
+        self.conn.close()
+
+test = Database()
+# test.intializeTables()
+# test.addContestant(12345, 'Toad', 'cranet@uw.edu', 'NONE')
+# test.addContestant(12890, 'Caleb', 'caleb447@uw.edu', 'NONE')
+# test.addContestant(01025, 'Alex', 'alamb25@uw.edu', 'NONE')
+# hi = test.getContestant(12345)
+# for i in hi:
+#     print i[1]
+# test.commit()
+# test.removeContestant('12345')
+# test.commit()
+# test.addContestant('12345', 'Toad', 'cranet@uw.edu', 'NONE')
+# hi = test.getContestant('12345')
+# print hi
+# test.modifyContestant('12345', ['1236', '1942'])
+# hi = test.getContestant('12345')
+# thing = []
+thing = set(test.getAllContestantID())
+for cont in thing:
+    print cont[0]
+# print hi
+test.closeDB()
