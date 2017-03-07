f1183c52698956851768b36918fa250a0f443cdd
cranet <cranet@uw.edu>
2017-03-06 21:25:03 -0800

    User class blocked

---
+++ User.py
@@ -0,0 +1,33 @@
+class User:
+    """User Class"""
+
+    def __init__(self, name, userID, email):
+        """Constructor"""
+        self.name = name
+        self.userID = userID
+        self.email = email
+
+   
+    def viewCategories():
+        """Takes user to categories page"""
+        pass
+
+
+    def viewJudges():
+        """Takes user to judges page"""
+        pass
+
+    
+    def viewSchedule():
+        """Takes user to schedule page"""
+        pass
+
+
+    def register():
+        """Takes user to register page"""
+        pass
+
+
+    def uploadPicture():
+        """Brings up the upload picture window"""
+        pass
