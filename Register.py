c2972c2ee5f05bab659cfaa37d0aaf6942971208
cranet <cranet@uw.edu>
2017-03-07 13:02:05 -0800

    Register GUI stubbed

---
+++ Register.py
@@ -0,0 +1,16 @@
+from Tkinter import *
+import tkMessageBox
+
+class Register(object):
+
+    def __init__(self):
+
+    
+    def regClick(self):
+        pass
+
+    def button_click(self, e):
+        pass
+
+
+Register()
