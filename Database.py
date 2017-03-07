9f70a2df56fbce39af045a16ce981ba44f39a7f3
RhaydenX <alamb25@uw.edu>
2017-03-07 03:00:08 -0800

    Demo of Contestant Database, THIS IS NOT PYUNIT TEST!

--- Database.py
+++ Database.py
@@ -12,10 +12,11 @@ class Database(object):
             UW NetID: alamb25\n
             Date: 3/7/17\n
             Constructor"""
+
         self.conn = sqlite3.connect('example.db')
         self.cursor = self.conn.cursor()
-        if not os.path.isfile('example.db'):
-            self.intializeTables()
+
+        self.intializeTables()
 
     def intializeTables(self):
         """ Author: Alex Lambert\n
@@ -23,25 +24,25 @@ class Database(object):
             Date: 3/7/17\n
             Initializes tables insuserIDe the Databse"""
 
-        self.cursor.execute('''CREATE TABLE CONTESTANTS
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS CONTESTANTS
                             (userID INT PRIMARY KEY     NOT NULL,
-                            NAME            TEXT    NOT NULL,
-                            EMAIL           TEXT    NOT NULL,
-                            CATEGORIES      TEXT    NOT NULL);''')
-        self.cursor.execute('''CREATE TABLE JUDGES
+                            NAME                TEXT    NOT NULL,
+                            EMAIL               TEXT    NOT NULL,
+                            CATEGORIES          TEXT    NOT NULL);''')
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS JUDGES
                             (userID INT PRIMARY KEY     NOT NULL,
-                            NAME            TEXT    NOT NULL,
-                            EMAIL           TEXT    NOT NULL,
-                            CATEGORIES      TEXT    NOT NULL);''')
-        self.cursor.execute('''CREATE TABLE ADMINS
+                            NAME                TEXT    NOT NULL,
+                            EMAIL               TEXT    NOT NULL,
+                            CATEGORIES          TEXT    NOT NULL);''')
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS ADMINS
                             (userID INT PRIMARY KEY     NOT NULL,
-                            NAME            TEXT    NOT NULL,
-                            EMAIL           TEXT    NOT NULL);''')
-        self.cursor.execute('''CREATE TABLE CATEGORIES
+                            NAME                TEXT    NOT NULL,
+                            EMAIL               TEXT    NOT NULL);''')
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS CATEGORIES
                             (userID INT PRIMARY KEY     NOT NULL,
-                            NAME            TEXT    NOT NULL,
-                            ABOUT           TEXT    NOT NULL,
-                            START_TIME      TEXT    NOT NULL);''')
+                            NAME                TEXT    NOT NULL,
+                            ABOUT               TEXT    NOT NULL,
+                            START_TIME          TEXT    NOT NULL);''')
 
     def commit(self):
         """ Author: Alex Lambert\n
@@ -49,14 +50,24 @@ class Database(object):
             Date: 3/7/17\n
             Commits changes to the Database\n
             To be used with the GUI"""
+
         self.conn.commit()
 
-    def addContestant(self, useruserID, name, email, categories):
+    def closeDB(self):
+        """ Author: Alex Lambert\n
+            UWnetID: alamb25\n
+            Date: 3/7/17\n
+            Closes the database\n
+            DOES NOT COMMIT CHANGES!"""
+
+        self.conn.close()
+
+    def addContestant(self, entry):
         """ Author: Alex Lambert\n
             UW NetID: alamb25\n
             Date: 3/7/17\n
             Add contestant to the Database"""
-        entry = [useruserID, name, email, categories]
+
         self.cursor.execute("INSERT INTO CONTESTANTS (userID,NAME,EMAIL,CATEGORIES) \
                             VALUES (?, ?, ?, ?)", entry)
 
@@ -65,7 +76,14 @@ class Database(object):
             UW NetID: alamb25\n
             Date: 3/7/17\n
             Gets contestant from the Database using unique userID"""
-        toReturn = self.cursor.execute('SELECT * FROM CONTESTANTS WHERE userID=?', (useruserID,))
+
+        temp = self.cursor.execute('SELECT * FROM CONTESTANTS WHERE userID=?', (useruserID,))
+        toReturn = []
+        for entry in temp:
+            toReturn.append(entry[0])
+            toReturn.append(entry[1])
+            toReturn.append(entry[2])
+            toReturn.append(entry[3].split('/'))
         return toReturn
 
     def getAllContestant(self):
@@ -73,7 +91,13 @@ class Database(object):
             UW NetID: alamb25\n
             Date: 3/7/17\n
             Returns all contestants from the Database"""
-        toReturn = self.cursor.execute('SELECT * FROM CONTESTANTS')
+
+        toReturn = []
+        temp = self.cursor.execute('SELECT * FROM CONTESTANTS')
+
+        for entry in temp:
+            tmp = entry[3].split('/')
+            toReturn.append([entry[0], entry[1], entry[2], tmp])
         return toReturn
 
     def getAllContestantuserID(self):
@@ -81,17 +105,23 @@ class Database(object):
             UW NetID: alamb25\n
             Date: 3/7/17\n
             Returns all contestants' unique userIDs from the Database"""
-        return set(self.cursor.execute('SELECT userID FROM contestants').fetchall())
 
-    def modifyContestant(self, userID, t):
+        toReturn = []
+        temp = self.cursor.execute('SELECT userID FROM contestants').fetchall()
+
+        for entry in temp:
+            toReturn.append(entry[0])
+        return set(toReturn)
+
+    def modifyContestant(self, userID, categories):
         """ Author: Alex Lambert\n
             UW NetID: alamb25\n
             Date: 3/7/17\n
             Modifies existing Contestant using the unique userID\n
             Changes the contestant's categories list"""
-        catList = " ".join(t)
-        te = [catList, userID]
-        self.cursor.execute('UPDATE set categories=? where userID=?', te)
+
+        temp = [categories, userID]
+        self.cursor.execute('UPDATE CONTESTANTS set categories=? where userID=?', temp)
 
     def removeContestant(self, userID):
         """ Author: Alex Lambert\n
@@ -99,22 +129,26 @@ class Database(object):
             Date: 3/7/17\n
             Removes the contestant corresponding to the unique userIDs from the Database\n
             Used for when a contestant gets moved to being a Judge"""
-        self.conn.execute("DELETE FROM contestants WHERE userID=?", (userID,))
+
+        self.conn.execute("DELETE FROM CONTESTANTS WHERE userID=?", (userID,))
 
     def addJudge(self, userID, name, email, categories):
         """ Author: Alex Lambert\n
             UW NetID: alamb25\n
             Date: 3/7/17\n
             Adds the judge to the database"""
+
         thing = [userID, name, email, categories]
-        self.cursor.execute("INSERT INTO contestants VALUES (?, ?, ?, ?)", thing)
+        self.cursor.execute("INSERT INTO CONTESTANTS VALUES (?, ?, ?, ?)", thing)
 
     def getJudge(self, userID):
         """ Author: Alex Lambert\n
             UW NetID: alamb25\n
             Date: 3/7/17\n
             Returns the judge from the Database using the unique id"""
-        self.cursor.execute('SELECT userID, name, email, categories FROM contestants WHERE userID=?', (userID,))
+
+        self.cursor.execute('SELECT userID, name, email, categories\
+                            FROM CONTESTANTS WHERE userID=?', (userID,))
         return self.cursor.fetchone()
 
     def removeJudge(self, userID):
@@ -122,12 +156,6 @@ class Database(object):
             UW NetID: alamb25\n
             Date: 3/7/17\n
             Returns all contestants from the Database"""
-        self.conn.execute("DELETE FROM contestants WHERE userID=?", (userID,))
 
-    def closeDB(self):
-        """ Author: Alex Lambert\n
-            UWnetID: alamb25\n
-            Date: 3/7/17\n
-            Closes the database\n
-            DOES NOT COMMIT CHANGES!"""
-        self.conn.close()
+        self.conn.execute("DELETE FROM CONTESTANTS WHERE userID=?", (userID,))
+
--- dbTest.py
+++ dbTest.py
@@ -5,4 +5,33 @@ from Database import *
 if __name__ == '__main__':
 
     testDB = Database()
+    testDB.addContestant([12345, 'Toad', 'cranet@uw.edu', 'NONE'])
+    testDB.addContestant([11025, 'Alex', 'alamb25@uw.edu', 'NONE/ONE/TWO'])
+    testDB.addContestant([24581, 'Caleb', 'caleb447@uw.edu', 'NONE/TWO'])
+
+    print '\n===GET CONTESTANT TEST==='
+    temp = testDB.getContestant(12345)
+    print temp[0], temp[1], temp[2], temp[3][0]
+
+    print '\n===MODIFY CONTESTANT TEST==='
+    testDB.modifyContestant(12345, 'NONE/THREE')
+    temp = testDB.getContestant(12345)
+    print temp[0], temp[1], temp[2], ' '.join(temp[3])
+
+    print '\n===GET ALL CONTESTANT TEST==='
+    temp = testDB.getAllContestant()
+    for row in temp:
+        print row[0], row[1], row[2], ' '.join(row[3])
+
+    print '\n===GET ALL IDs TEST TEST==='
+    temp = testDB.getAllContestantuserID()
+    for row in temp:
+        print row
+
+    print '\n===REMOVE TEST==='
+    testDB.removeContestant(12345)
+    temp = testDB.getAllContestant()
+    for row in temp:
+        print row[0], row[1], row[2], ' '.join(row[3])
+
 
