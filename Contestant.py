96a7149095f06b9fe68c18e5a58359f01775a5e2
cranet <cranet@uw.edu>
2017-03-06 23:49:45 -0800

    Contestant stubbed

--- Contestant.py
+++ Contestant.py
@@ -1,3 +1,25 @@
 """Contestant class"""
 class Contestant(object):
     """Contestant class"""
+
+    def regCategory(self):
+        """Registers contestant in chosen category"""
+        pass
+
+    def dropCategory(self):
+        """Drops contestant from chosen category"""
+        pass
+
+    def requestJudge(self):
+        """Marks the contestant as possible judge"""
+        pass
+
+    def uploadPicture(self):
+        """Uploads contestant picture"""
+        pass
+
+"""Main"""
+if __name__ == '__main__':
+    test = Contestant()
+
+
