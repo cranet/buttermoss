558cdbcdf3e617a485cac9cca0171b1afe81e719
cranet <cranet@uw.edu>
2017-03-17 17:50:39 -0700

    Cleaned

--- HomePage.py
+++ HomePage.py
@@ -1,9 +1,8 @@
-import Tkinter as tk   
-
-from BeweeveMain import DATABASE, CURRENT_USER
+"""Home Page"""
+import Tkinter as tk
+from BeweeveMain import CURRENT_USER #DATABASE
 
 TITLE_FONT = ("Helvetica", 20, "bold")
-
 
 class HomePage(tk.Frame):
     """Author: Thomas Schmidt\n
@@ -16,26 +15,19 @@ class HomePage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        #self.update()
 
         label = tk.Label(self, text="Welcome!", font=TITLE_FONT)
         label.pack(side="top", fill="x", pady=10)
 
-        #str = 
-
         idLabel = tk.Label(self, text="ID: %i" %(CURRENT_USER.userID))
         idLabel.pack()
 
-        
-        button2 = tk.Button(self, text = "Event Schedule", width=12, command=lambda: controller.show_frame("EventSchedulePage")) 
-        button4 = tk.Button(self, text = "Judges", width=12, command=lambda: controller.show_frame("JudgesPage"))
-
-
+        button2 = tk.Button(self, text="Event Schedule", width=12,
+                            command=lambda: controller.show_frame("EventSchedulePage"))
+        button4 = tk.Button(self, text="Judges", width=12,
+                            command=lambda: controller.show_frame("JudgesPage"))
         button4.pack(pady=5)
         button2.pack(pady=5)
-    
-
         button = tk.Button(self, text="Logout", width=12,
                            command=lambda: controller.show_frame("LoginPage"))
         button.pack()
-
