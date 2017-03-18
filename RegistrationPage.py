b316a2974801ff1c813c3f0c84718b74bac22566
cranet <cranet@uw.edu>
2017-03-17 18:04:23 -0700

    Cleaned

--- RegistrationPage.py
+++ RegistrationPage.py
@@ -1,5 +1,5 @@
-import Tkinter as tk   
-
+"""Registration Page"""
+import Tkinter as tk
 from BeweeveMain import DATABASE, CURRENT_USER
 
 TITLE_FONT = ("Helvetica", 20, "bold")
@@ -10,7 +10,8 @@ class RegistrationPage(tk.Frame):
     Author: Evan Pernu\n
     UW NetID: epernu\n
     Date: 3/11/2017\n
-    This is the RegistrationPage Class that will show the registration page once the user attempts to register."""
+    This is the RegistrationPage Class that will show the
+    registration page once the user attempts to register."""
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -33,12 +34,13 @@ class RegistrationPage(tk.Frame):
                            command=lambda: self.regClick(controller))
         button.pack()
 
-        backButton = tk.Button(self, text='Back', 
+        backButton = tk.Button(self, text='Back',
                                command=lambda: controller.show_frame("HomePage"))
 
         backButton.pack()
 
     def regClick(self, controller):
+        """Gets user input, sends to database"""
 
         #Need non string exception
         if self.nameText.get() != "" or self.emailText.get() != "":
@@ -48,16 +50,15 @@ class RegistrationPage(tk.Frame):
             name = self.nameText.get()
             email = self.emailText.get()
 
-            CURRENT_USER.userID = self.sendToDatabase(name, email)    #added by Alex Lambert 3/9/17
-            DATABASE.commit()                            #added by Alex Lambert 3/9/17  
+            CURRENT_USER.userID = self.sendToDatabase(name, email) #Aadded by Alex Lambert 3/9/17
+            DATABASE.commit() #Added by Alex Lambert 3/9/17
 
             self.pop = tk.Tk()
             self.pop.wm_title("User ID")
             label = tk.Label(self.pop, text='Your Unique ID is:\n'+str(CURRENT_USER.userID))
             label.pack(fill="x", pady=10)
-            button = tk.Button(self.pop, text="Okay", command=lambda: self.daisy())
+            button = tk.Button(self.pop, text="Okay", command=lambda: self.frameChange())
             button.pack()
-
         else:
             self.errorPop = tk.Tk()
             self.errorPop.wm_title("ERROR")
@@ -65,12 +66,11 @@ class RegistrationPage(tk.Frame):
             self.errorLabel.pack()
             self.errorButton = tk.Button(self.errorPop, text="Okay", command=self.errorPop.destroy)
             self.errorButton.pack(pady=5)
-            #controller.show_frame("RegistrationPage")
 
-    def daisy(self):
+    def frameChange(self):
+        """Destroys popup and changes frame page"""
         self.pop.destroy()
         self.controller.show_frame("HomePage")
-
 
     def sendToDatabase(self, name, email):
         """ Author: Alex Lambert\n
@@ -80,7 +80,3 @@ class RegistrationPage(tk.Frame):
 
         entry = [name, email, '']
         return  DATABASE.addContestant(entry)
-
-    def displayUserID(self, userID):
-        userID = tk.Label(self, text=userID)
-        userID.pack()
